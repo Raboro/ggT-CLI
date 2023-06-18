ce74fc41e607efbacad0451245e3c5f95192a028
Raboro <raborogit@gmail.com>
2023-06-18 17:12:24 +0200

    made CLI accessible with numbers as input

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@
 
 "use strict"
 
-console.log("ggt")
+const yargs = require('yargs');
+
+const options = yargs
+    .option("v", {
+        alias: "v",
+        describe: "values for the ggt calculation",
+        type: "array",
+        coerce: (arg) => arg.map(Number)
+    })
+    .command("$0 [v..]", "values for ggt calculation")
+    .argv;
+
+console.log(options.v);
